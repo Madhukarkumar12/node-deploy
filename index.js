7d7d7e6bcd72208ad366bae3693af8b35feb2502
Madhukar Kumar <150505032+Madhukarkumar12@users.noreply.github.com>
2026-01-30 18:48:26 +0530

    initial setup

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT ?? 8081;
+
+app.get('/', (req,res) => {
+    return res.json({msg:'Hello From Madhukar...'})
+})
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on PORT ${PORT}`);
+})
